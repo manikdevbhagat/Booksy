86eebf2576594b6e06ab691d1ca56fdfb4ce6b4e
manikdevbhagat <manikdevbhagat@gmail.com>
2023-11-25 16:58:20 +0530

    Created authors page

--- frontend/src/pages/Authors.tsx
+++ frontend/src/pages/Authors.tsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
+import authors from "../assets/data/authors";
+import AuthorCard from "../components/Authors/AuthorCard";
+import AuthorDescription from "../components/Authors/AuthorDescription";
 
 const Authors = () => {
-  return (
-    <div>Authors</div>
-  )
-}
+  const [showDescription, setShowDescription] = useState<Boolean>(false);
+  const [authorId, setAuthorId] = useState<number | null>(null);
 
-export default Authors
+  const author = authors.filter((author) => author.id === authorId)[0];
+
+  return (
+    <div className="relative">
+      {showDescription && (
+        <>
+          <div className="absolute w-full h-full bg-gray-200 bg-opacity-50 z-10"></div>
+          <AuthorDescription
+            author={author}
+            setShowDescription={setShowDescription}
+          />
+        </>
+      )}
+      <section>
+        <h1 className="heading text-center">Meet our talented authors</h1>
+      </section>
+      <section>
+        <div className="flex flex-wrap gap-10 justify-around px-[50px] md:px-[150px]">
+          {authors.map((author) => (
+            <div
+              onClick={() => {
+                setAuthorId(author.id);
+                setShowDescription(true);
+              }}
+            >
+              <AuthorCard key={author.id} author={author} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Authors;
