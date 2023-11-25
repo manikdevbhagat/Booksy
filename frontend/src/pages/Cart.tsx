41bee5d69e130d3f6f092ceeb0b780f04e23ebfc
manikdevbhagat <manikdevbhagat@gmail.com>
2023-11-25 17:00:03 +0530

    Created cart page

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,9 +1,36 @@
-
+import CartItem from "../components/Cart/CartItem";
+import CartTotal from "../components/Cart/CartTotal";
+import { useAppSelector } from "../hooks/reduxhooks";
 
 const Cart = () => {
+  const { books } = useAppSelector((state) => state.cart);
+  let totalAmount = 0;
+  for(const el of books){
+    totalAmount = totalAmount + el.book.price*el.quantity;
+  }
   return (
-    <div>Cart</div>
-  )
-}
+    <section className="min-h-screen">
+      <h1 className="heading text-center">Cart Items</h1>
+      {books.length === 0 ? (
+        <h1 className="text-textColor mt-12 font-semibold text-2xl text-center">{`Cart is empty :-(`}</h1>
+      ) : (
+        <section className="px-[50px]">
+          <div className="flex flex-col md:flex-row w-full justify-around">
+            {/*cart items*/}
+            <div>
+              {books.map((book) => (
+                <CartItem bookAndQuantity={book} />
+              ))}
+            </div>
+            {/*total*/}
+            <div>
+              <CartTotal total={totalAmount}/>
+            </div>
+          </div>
+        </section>
+      )}
+    </section>
+  );
+};
 
-export default Cart
+export default Cart;
